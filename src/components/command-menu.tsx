4686fd96e7b2e612474ff85820e0aff8f6be8309
Jeferson Barrero <jebarrero14@gmail.com>
2023-12-26 11:30:46 -0500

    Add UI components

---
+++ src/components/command-menu.tsx
@@ -0,0 +1,70 @@
+"use client";
+import { LaptopIcon, MoonIcon, SunIcon } from "lucide-react";
+import { useTheme } from "next-themes";
+import { useCallback, useEffect, useState } from "react";
+import {
+  Button,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "./ui";
+
+export const CommandMenu = () => {
+  const [open, setOpen] = useState(false);
+  const { setTheme } = useTheme();
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
+  const runCommand = useCallback((command: () => unknown) => {
+    setOpen(false);
+    command();
+  }, []);
+  return (
+    <>
+      <Button
+        variant="outline"
+        onClick={() => setOpen(true)}
+        className="relative h-8 w-full justify-start rounded-[0.5rem] bg-background text-sm font-normal text-muted-foreground shadow-none sm:pr-12 md:w-40 lg:w-64"
+      >
+        <span className="inline-flex ">Search...</span>
+        <kbd className="pointer-events-none absolute right-[0.3rem] top-[0.3rem] hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
+          <span className="text-xs">⌘</span>K
+        </kbd>
+      </Button>
+
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="buscar ..." />
+        <CommandList>
+          <CommandEmpty>No hay resultados para su busqueda</CommandEmpty>
+          <CommandGroup heading="Theme">
+            <CommandItem onSelect={() => runCommand(() => setTheme("light"))}>
+              <SunIcon className="mr-2 h-4 w-4" />
+              Light
+            </CommandItem>
+            <CommandItem onSelect={() => runCommand(() => setTheme("dark"))}>
+              <MoonIcon className="mr-2 h-4 w-4" />
+              Dark
+            </CommandItem>
+            <CommandItem onSelect={() => runCommand(() => setTheme("system"))}>
+              <LaptopIcon className="mr-2 h-4 w-4" />
+              System
+            </CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+    </>
+  );
+};
---
+++ src/components/header.tsx
@@ -0,0 +1,30 @@
+"use client";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import { Theme } from "./theme";
+
+export const Header = () => {
+  return (
+    <header className="w-full h-20 px-6 py-1 fixed bg-gradient-to-t to-background from-transparent z-20">
+      <div className="flex justify-between items-center w-full h-full">
+        <div>
+          <Link href="/" className="font-mono text-2xl capitalize">
+            Votaciones escolares
+          </Link>
+        </div>
+        <div className="flex gap-4">
+          <Theme />
+          <Link
+            href="/signin"
+            className={buttonVariants({ variant: "outline" })}
+          >
+            Iniciar sesion
+          </Link>
+          <Link href="/signup" className={buttonVariants()}>
+            Crear cuenta
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
